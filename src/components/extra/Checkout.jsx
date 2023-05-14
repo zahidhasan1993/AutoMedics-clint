de3727c8169e0027a74a929a16b563cf017a4b12
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-15 01:50:14 +0600

    styling and adding from function to checkout page

--- src/components/extra/Checkout.jsx
+++ src/components/extra/Checkout.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Checkout = () => {
   const service = useLoaderData();
-  const { _id,img,description,title,price } = service;
+  const { _id, img, description, title, price } = service;
 
-//   console.log(service);
+  //   console.log(service);
+  const handleOrder = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const date = form.date.value;
+    const amount = form.amount.value;
+
+    const order = {
+      customerName: name,
+      email,
+      date,
+      payingAmount: amount,
+    };
+
+    console.log(order);
+  };
 
   return (
-    <div>
+    <div className="mb-12">
       <div
-        className="hero h-[18.7rem] mb-14"
+        className="hero h-[18.7rem] mb-32"
         style={{
           backgroundImage: `url("${img}")`,
         }}
@@ -20,6 +37,62 @@ const Checkout = () => {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Checkout</h1>
           </div>
+        </div>
+      </div>
+      <div>
+        <div className="card  w-full shadow-2xl bg-base-100">
+          <form
+            onSubmit={handleOrder}
+            className="card-body md:grid md:grid-cols-2 md:gap-6"
+          >
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <input type="date" name="date" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Paying Amount</span>
+              </label>
+              <input
+                type="text"
+                name="amount"
+                placeholder="00"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control mt-6 md:col-span-2 ">
+              <input
+                className="btn btn-outline text-orange-700 hover:bg-orange-700 hover:border-none hover:text-white"
+                type="submit"
+                value="Order Confirm"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </div>
