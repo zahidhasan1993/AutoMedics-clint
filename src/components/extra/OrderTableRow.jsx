7dda9784e50862e545fc6bc9b96128954583965c
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-15 23:09:50 +0600

    adding delete functionality with server to delete a single order

--- src/components/extra/OrderTableRow.jsx
+++ src/components/extra/OrderTableRow.jsx
@@ -1,28 +1,18 @@
 import React from "react";
 
-const OrderTableRow = ({data}) => {
-//   console.log(data);
-  const {img,payingAmount,service_price,service_title,date,email} = data;
+const OrderTableRow = ({ data , handleDelete }) => {
+  //   console.log(data);
+  const { _id,img, payingAmount, service_price, service_title, date, email } = data;
 
-  const dueAmount = Number(service_price) - Number(payingAmount); 
- 
+  const dueAmount = Number(service_price) - Number(payingAmount);
 
-
-    return (
+  return (
     <tr>
-      <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
-      </th>
       <td>
         <div className="flex items-center space-x-3">
           <div className="avatar">
             <div className="mask mask-squircle w-12 h-12">
-              <img
-                src="/tailwind-css-component-profile-2@56w.png"
-                alt="Avatar Tailwind CSS Component"
-              />
+              <img src={img} alt="Avatar Tailwind CSS Component" />
             </div>
           </div>
           <div>
@@ -31,12 +21,28 @@ const OrderTableRow = ({data}) => {
           </div>
         </div>
       </td>
-      <td>
-        ${payingAmount}
-      </td>
+      <td>${payingAmount}</td>
       <td>${dueAmount}</td>
       <th>
         <button className="btn btn-ghost btn-xs">{date}</button>
+      </th>
+      <th>
+        <button onClick={() => handleDelete(_id)} className="btn btn-square btn-outline text-orange-700 hover:bg-orange-700 hover:text-white hover:border-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </th>
     </tr>
   );
--- src/components/extra/Orders.jsx
+++ src/components/extra/Orders.jsx
@@ -1,20 +1,50 @@
 import React, { useContext, useEffect, useState } from "react";
-import img from "../../assets/images/banner/6.jpg"
+import img from "../../assets/images/banner/6.jpg";
 import { DataProvider } from "../Providers/AuthProvider";
 import OrderTableRow from "./OrderTableRow";
+import Swal from "sweetalert2";
+
+import { data } from "autoprefixer";
 
 const Orders = () => {
-    const [orders,setOrders] = useState([])
-    const {user} = useContext(DataProvider); 
+  const [orders, setOrders] = useState([]);
+  const { user } = useContext(DataProvider);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/orders?email=${user?.email}`)
-        .then(res => res.json())
-        .then(data => setOrders(data))
-    },[])
 
-    // console.log(orders);
-    
+  useEffect(() => {
+    fetch(`http://localhost:5000/orders?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
+  const handleDelete = (id) => {
+    // console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/orders/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.acknowledged) {
+              const remaining = orders.filter(order => order._id !== id);
+              // console.log(remaining);
+              setOrders(remaining)
+            }
+          });
+      }
+    });
+  };
+
+  // console.log(orders);
+
   return (
     <div>
       <div
@@ -33,31 +63,31 @@ const Orders = () => {
 
       {/* table */}
       <div className="overflow-x-auto w-full mb-32">
-  <table className="table w-full">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Service Name</th>
-        <th>paying Amount</th>
-        <th>Due Amount</th>
-        <th>Date</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        orders.map(order => <OrderTableRow key={order._id} data={order}></OrderTableRow>)
-      }
-      
-    </tbody>
-  
-    
-  </table>
-</div>
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              
+              <th>Service Name</th>
+              <th>paying Amount</th>
+              <th>Due Amount</th>
+              <th>Date</th>
+              <th>
+                Delete Order
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <OrderTableRow
+                key={order._id}
+                data={order}
+                handleDelete={handleDelete}
+              ></OrderTableRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
