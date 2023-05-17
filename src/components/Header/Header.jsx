75f9a39d0e3c44b2e27ae70a60b2f346745f57ce
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-17 17:19:36 +0600

    some styling changes for mobile devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
           src={pic2}
           className="w-full"
         />
-        <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full">
+        <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full  p-7 md:p-0">
             <div className="md:pl-24 md:pt-24">
             <h1 className="text-6xl text-white font-bold">
             Affordable <br /> Price For Car <br /> Servicing
@@ -66,7 +66,7 @@ const Header = () => {
           src={pic3}
           className="w-full"
         />
-        <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full">
+        <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full  p-7 md:p-0">
             <div className="md:pl-24 md:pt-24">
             <h1 className="text-6xl text-white font-bold">
             Affordable <br /> Price For Car <br /> Servicing
@@ -92,7 +92,7 @@ const Header = () => {
           src={pic4}
           className="w-full"
         />
-       <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full">
+       <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full  p-7 md:p-0">
             <div className="md:pl-24 md:pt-24">
             <h1 className="text-6xl text-white font-bold">
             Affordable <br /> Price For Car <br /> Servicing
@@ -118,7 +118,7 @@ const Header = () => {
           src={pic5}
           className="w-full"
         />
-       <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full">
+       <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full  p-7 md:p-0">
             <div className="md:pl-24 md:pt-24">
             <h1 className="text-6xl text-white font-bold">
             Affordable <br /> Price For Car <br /> Servicing
@@ -144,7 +144,7 @@ const Header = () => {
           src={pic6}
           className="w-full"
         />
-       <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full">
+       <div className="absolute left-0 top-0 h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] w-full  p-7 md:p-0">
             <div className="md:pl-24 md:pt-24">
             <h1 className="text-6xl text-white font-bold">
             Affordable <br /> Price For Car <br /> Servicing
